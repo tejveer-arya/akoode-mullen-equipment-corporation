144a36da766800ae028bccaa00e97ed4f6cfd5b4
Tejveer <tejvirarya@gmail.com>
2024-10-19 01:19:43 +0530

    file and css updated

---
+++ inc/footer.php
@@ -0,0 +1,134 @@
+ <!--footer start-->
+        
+ <footer class="footer dark-bg position-relative" data-bg-img="images/bg/09.png" style="background-size: contain; background-repeat: no-repeat;">
+          <div class="footer-contact">
+            <div class="container">
+              <div class="row">
+                <div class="col">
+                  <div class="theme-bg">
+                    <div class="row g-0">
+                      <div class="col-md-4 mb-5 mb-md-0">
+                        <div class="px-5 pb-5 text-center footer-media"> <i class="fas fa-map-marker-alt"></i>
+                          <span>Address:</span>
+                          <p class="mb-0">Troy, Michigan</p>
+                        </div>
+                      </div>
+                      <div class="col-md-4 mb-5 mb-md-0">
+                        <div class="px-5 pb-5 text-center footer-media"> <i class="flaticon-phone-call"></i>
+                          <span>Phone:</span>
+                          <a href="tel:+1-2486438120">(248) 643 8120</a>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="px-5 pb-5 text-center footer-media border-0"> <i class="fas fa-envelope"></i>
+                          <span>Email:</span>
+                          <a href="mailto:Mulleneq@mullenequipment.com">Mulleneq@mullenequipment.com</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="primary-footer">
+            <div class="container">
+              <div class="row text-center">
+                <div class="col-lg-8 mx-auto">
+                  <h5>About<span class="text-theme"> Mullen Equipment</span></h5>
+                  <p class="mb-3">Based in Troy, Michigan, Mullen Equipment has been a successful industrial equipment representative and chemical equipment distributor in the Midwest since 1950. Generating significant sales for our principals in the Chemical industry products sector, Pharmaceutical process solutions, Petrochemical industry, Industrial sector services, Municipal solutions, Food & Beverage, Paper & Pulp and various other industries. Today, we continue to expand our reach and areas of expertise through excellent service to our customers and principals.</p> 
+                </div>
+                <div class="col-lg-8 mx-auto mt-5 mt-md-0 footer-list">
+                  <ul class="list-unstyled">
+                    <li><a href="javascript:void(0)" class="mx-4">Home</a>
+                    </li>
+                    <li><a href="javascript:void(0)" class="mx-4">About Us</a>
+                    </li>
+                    <li><a href="javascript:void(0)" class="mx-4">Products</a>
+                    </li>
+                    <li><a href="javascript:void(0)" class="mx-4">FAQs</a>
+                    </li>
+                    <li><a href="javascript:void(0)" class="mx-4">Blog</a>
+                    </li>
+                    <li><a href="javascript:void(0)" class="mx-4">Contact Us</a>
+                    </li>
+                  </ul>
+                  <div class="social-icons social-colored mt-3">
+                    <ul class="list-inline mb-0">
+                      <li class="social-facebook"><a href="javascript:void(0)"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+                      </li>
+                      <li class="social-twitter"><a href="javascript:void(0)"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+                      </li>
+                      <li class="social-gplus"><a href="javascript:void(0)"><i class="fab fa-google-plus-g" aria-hidden="true"></i></a>
+                      </li>
+                      <li class="social-linkedin"><a href="javascript:void(0)"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                
+                
+                  
+                
+              </div>
+            </div>
+          </div>
+          <div class="secondary-footer">
+            <div class="container">
+              <div class="copyright">
+                <div class="row align-items-center">
+                  <div class="col-md-7">
+                   <span>Copyright 2024 Mullen Equipment Corporation | Made With <a target="_blank" href="https://www.akoode.com/"> <i class="fas fa-heart"></i> </a></span>
+                  </div>
+                  <div class="col-md-5 text-md-end mt-3 mt-md-0 footer-list">
+                    <ul class="list-inline">
+                      <li><a href="javascript:void(0)">Terms & Condition</a></li>
+                      <li><a href="javascript:void(0)">Privacy Policy</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+        
+        <!--footer end-->
+        
+        
+        </div>
+        
+        <!-- page wrapper end -->
+        
+        
+        <!-- get a quote start -->
+        
+        <div class="contact-form text-black"> <a class="close-btn text-capitalize text-end"><i class="flaticon-cancel"></i></a>
+          <h2 class="title mb-5">Get in <span> Touch </span></h2>
+          <form id="queto-form">
+            <div class="messages"></div>
+            <div class="form-group">
+              <input id="form_name1" type="text" name="name" class="form-control" placeholder="Your Name">
+              
+            </div>
+            <div class="form-group">
+              <input id="form_email1" type="email" name="email" class="form-control" placeholder="Your Email">
+             
+            </div>
+            <div class="form-group">
+              <input id="form_project1" type="text" name="name" class="form-control" placeholder="Project Name">
+            </div>
+            <div class="form-group">
+              <textarea id="form_message1" name="message" class="form-control" placeholder="Your Message" rows="3"></textarea>
+             
+            </div>
+            <button class="btn btn-border btn-radius"><span>Submit</span>
+            </button>
+          </form>
+        </div>
+        
+        <!-- get a quote end -->
+        
+        
+        <!--back-to-top start-->
+        
+        <div class="scroll-top"><a class="smoothscroll" href="#top"><i class="fas fa-chevron-up"></i></a></div>
